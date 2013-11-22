ab522d287ee21a8614dccd5562820bd35ae74813
Erno Aapa <erno.aapa@avaus.fi>
2013-11-22 07:19:12 +0200

    Moved dns configuration attributes to own attribute and extracted nopoll and noresolv attributes out.
    To be able to "not set" no-poll and no-resolv attributes there wasn't way
    to do it previously. After this commit you can set nopoll and noresolv
    attributes to false and it will not add those lines to conf -file.
    
    Configuration have moved from node[:dnsmasq][:dns]
    to node[:dnsmasq][:dns][:config] so this is not backward compatible.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,10 @@ default[:dnsmasq][:enable_dns] = true
 default[:dnsmasq][:enable_dhcp] = false
 default[:dnsmasq][:managed_hosts] = {}
 default[:dnsmasq][:managed_hosts_bag] = "managed_hosts"
-default[:dnsmasq][:dns] = {
-  'no-poll' => nil,
-  'no-resolv' => nil,
+
+default[:dnsmasq][:dns][:nopoll] = true
+default[:dnsmasq][:dns][:noresolv] = true
+default[:dnsmasq][:dns][:config] = {
   'server' => '127.0.0.1'
 }
 default[:dnsmasq][:dhcp] = {}
--- recipes/dns.rb
+++ recipes/dns.rb
@@ -1,9 +1,17 @@
 include_recipe 'dnsmasq::default'
 include_recipe 'dnsmasq::manage_hostsfile'
 
-dns_config = node[:dnsmasq][:dns].to_hash
+dns_config = node[:dnsmasq][:dns][:config].to_hash
 unless(node[:dnsmasq][:enable_dhcp])
   dns_config['no-dhcp-interface='] = nil
+end
+
+if node[:dnsmasq][:dns][:nopoll]
+  dns_config["no-poll"] = nil
+end
+
+if node[:dnsmasq][:dns][:noresolv]
+  dns_config["no-resolv"] = nil
 end
 
 template '/etc/dnsmasq.d/dns.conf' do
